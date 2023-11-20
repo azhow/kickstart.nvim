ac925eff9b66ce1c0975518415d1e7e72eb5d8f8
Arthur Zachow <arthur.zachow.dev@gmail.com>
2023-11-20 23:37:09 +0100

    updating telescope config for adding preview

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -1,3 +1,10 @@
 return {
-  "nvim-telescope/telescope.nvim"
+  "nvim-telescope/telescope.nvim",
+  defaults = {
+    previewer = true,
+    file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+    grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+    qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+    layout_strategy = "flex",
+  }
 }
